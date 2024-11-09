bebda4a5a2747a03d8e0effcd13985649aa07133
antonvzar <antonvzar@gmail.com>
2024-11-09 17:14:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to enable smooth scrolling between sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
